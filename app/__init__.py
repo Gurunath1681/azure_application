0e89c4b0a76a8eb65f087cb16e3b8d79e7eb489b
Gurunath1681 <gurunath.sharma@gmail.com>
2023-04-10 23:13:14 +0530

    api, profiles

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_restful import Api
 
+from .profiles import profiles_blueprint
+from .api.profile import ProfileResource
 
 app = Flask(__name__)
+api = Api(app, prefix="/api")
+app.register_blueprint(profiles_blueprint)
+api.add_resource(ProfileResource, "/profiles")
 
 
 @app.route('/')
---
+++ app/api/profile.py
@@ -0,0 +1,4 @@
+from flask_restful import Resource
+class ProfileResource(Resource):
+    def get(self):
+        return {"profile": "resource"}
